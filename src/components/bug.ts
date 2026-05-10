fbbee50a5b9a092b50f6ee8cfd823f56eac9d987
Alex Houseago <ahouseago@gmail.com>
2026-05-10 21:07:18 +0100

    Bug is a class now

---
+++ src/components/bug.ts
@@ -0,0 +1,62 @@
+import { Point, Sprite, Texture } from "pixi.js";
+
+type BugProperties = {
+  velocity: Point;
+  maxSpeed: number;
+  acceleration: number;
+};
+
+export class Bug extends Sprite {
+  velocity: Point;
+  maxSpeed: number;
+  acceleration: number;
+
+  constructor(texture: Texture, props: BugProperties) {
+    super(texture);
+
+    // Center the sprite's anchor point
+    this.anchor.set(0.5);
+
+    this.velocity = props.velocity;
+    this.maxSpeed = props.maxSpeed;
+    this.acceleration = props.acceleration;
+  }
+
+  moveTowards(target: Point, deltaTime: number) {
+    const targetRotation =
+      Math.atan2(target.y - this.y, target.x - this.x) + Math.PI / 2;
+    const rawDiff = targetRotation - this.rotation;
+    const shortestDiff = Math.atan2(Math.sin(rawDiff), Math.cos(rawDiff));
+    this.rotation = this.rotation + shortestDiff * 0.05 * deltaTime;
+
+    const dx = target.x - this.position.x;
+    const dy = target.y - this.position.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > Math.max(this.width / 2, this.height / 2)) {
+      const angle = this.rotation - Math.PI / 2;
+      this.velocity.x += Math.cos(angle) * this.acceleration;
+      this.velocity.y += Math.sin(angle) * this.acceleration;
+      const currentSpeed = Math.sqrt(
+        this.velocity.x * this.velocity.x + this.velocity.y * this.velocity.y,
+      );
+
+      if (currentSpeed > this.maxSpeed) {
+        this.velocity.x = (this.velocity.x / currentSpeed) * this.maxSpeed;
+        this.velocity.y = (this.velocity.y / currentSpeed) * this.maxSpeed;
+      }
+
+      // If the distance to the target is less than our speed this frame snap to it.
+      if (distance <= currentSpeed) {
+        this.position.x = target.x;
+        this.position.y = target.y;
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+      } else {
+        // Otherwise, apply the velocity to move the this
+        this.position.x += this.velocity.x * deltaTime;
+        this.position.y += this.velocity.y * deltaTime;
+      }
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Application, Assets, Rectangle, Sprite } from "pixi.js";
+import { Application, Assets, Point, Rectangle, Sprite } from "pixi.js";
+import { Bug } from "./components/bug";
 
 (async () => {
   // Create a new application
@@ -14,12 +15,13 @@ import { Application, Assets, Rectangle, Sprite } from "pixi.js";
   const texture = await Assets.load("/assets/bug.svg");
 
   // Create a bug Sprite
-  const bug = new Sprite(texture);
+  const bug = new Bug(texture, {
+    velocity: new Point(),
+    maxSpeed: 1,
+    acceleration: 0.4,
+  });
 
-  // Center the sprite's anchor point
-  bug.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
+  // Move the bug to the centre of the screen
   bug.position.set(app.screen.width / 2, app.screen.height / 2);
 
   // Add the bug to the stage
@@ -28,67 +30,17 @@ import { Application, Assets, Rectangle, Sprite } from "pixi.js";
   app.stage.eventMode = "static";
   app.stage.hitArea = new Rectangle(0, 0, app.screen.width, app.screen.height);
 
-  // Variables to remember where we want to go
-  let targetX = bug.x;
-  let targetY = bug.y;
-  let vx = 0;
-  let vy = 0;
-  const acceleration = 0.4;
-  const maxSpeed = 1;
-
-  let isMoving = false;
-  app.stage.on("pointerdown", (event) => {
-    targetX = event.global.x;
-    targetY = event.global.y;
-    isMoving = true;
+  let mouseIsDown = false;
+  app.stage.on("pointerdown", () => {
+    mouseIsDown = true;
   });
   app.stage.on("pointerup", () => {
-    isMoving = false;
-    vx = 0;
-    vy = 0;
-  });
-  app.stage.on("pointermove", (e) => {
-    if (isMoving) {
-      targetX = e.global.x;
-      targetY = e.global.y;
-    }
+    mouseIsDown = false;
   });
 
   app.ticker.add((time) => {
-    const current = bug.rotation;
-    const cursor = app.renderer.events.pointer.global;
-    const target = Math.atan2(cursor.y - bug.y, cursor.x - bug.x) + Math.PI / 2;
-    const rawDiff = target - current;
-    const shortestDiff = Math.atan2(Math.sin(rawDiff), Math.cos(rawDiff));
-    bug.rotation = current + shortestDiff * 0.05 * time.deltaTime;
-
-    if (isMoving) {
-      const dx = targetX - bug.x;
-      const dy = targetY - bug.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-      if (distance > 0) {
-        const angle = bug.rotation - Math.PI / 2;
-        vx += Math.cos(angle) * acceleration;
-        vy += Math.sin(angle) * acceleration;
-        const currentSpeed = Math.sqrt(vx * vx + vy * vy);
-
-        if (currentSpeed > maxSpeed) {
-          vx = (vx / currentSpeed) * maxSpeed;
-          vy = (vy / currentSpeed) * maxSpeed;
-        }
-
-        // If the distance to the target is less than our speed this frame snap to it.
-        if (distance <= currentSpeed) {
-          bug.x = targetX;
-          bug.y = targetY;
-          vx = 0;
-          vy = 0;
-        } else {
-          // Otherwise, apply the velocity to move the bug
-          bug.x += vx;
-          bug.y += vy;
-        }
-      }
+    if (mouseIsDown) {
+      bug.moveTowards(app.renderer.events.pointer.global, time.deltaTime);
     }
   });
 })();
